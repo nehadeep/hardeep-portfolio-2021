0e793ce62142c9d1eb1f2b3961be38c147be22a5
nehadeep <erdeepjaswal@gmail.com>
2020-12-01 12:01:08 -0800

    displayed the portfolio data on portfolio page with axios

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,6 +1,29 @@
+import axios from 'axios';
+import PortfolioCard from "@/components/portfolios/PortfolioCard";
 
 
-const Portfolios = () =>{
+
+const fetchPortfolios = () =>{
+    const query = `query Portfolios { 
+          portfolios {
+            _id, 
+            title, 
+            company,
+            companyWebsite,
+            location,
+            jobTitle,
+            description,
+            startDate,
+            endDate
+            }
+          }  
+        `;
+
+    return axios.post('http://localhost:3000/graphql', {query}).
+    then(({data: graph})=> graph.data).then(data=> data.portfolios)
+};
+const Portfolios = ({portfolios}) =>{
+
     return (
         <>
                 <section className="section-title">
@@ -12,50 +35,20 @@ const Portfolios = () =>{
                 </section>
                 <section className="pb-5">
                     <div className="row">
-                        <div className="col-md-4">
-                            <div className="card subtle-shadow no-border">
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                                    <p className="card-text fs-2">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                </div>
-                                <div className="card-footer no-border">
-                                    <small className="text-muted">Last updated 3 mins ago</small>
-                                </div>
+                        { portfolios.map(portfolio=>
+                            <div className="col-md-4" key={portfolio._id}>
+                                <PortfolioCard portfolio={portfolio}/>
                             </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className="card subtle-shadow no-border">
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                                    <p className="card-text fs-2 ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                </div>
-                                <div className="card-footer no-border">
-                                    <small className="text-muted">Last updated 3 mins ago</small>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className="card subtle-shadow no-border">
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                                    <p className="card-text fs-2 ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                </div>
-                                <div className="card-footer no-border">
-                                    <small className="text-muted">Last updated 3 mins ago</small>
-                                </div>
-                            </div>
-                        </div>
+                        )}
                     </div>
                 </section>
         </>
     )
 };
 
-// Portfolios.getInitialProps = () =>{
-//
-// }
+Portfolios.getInitialProps = async () =>{
+const portfolios = await fetchPortfolios();
+  return {portfolios};
+};
 
 export default Portfolios;
