9072de856c5b6a15d5274c02a4388fc6d4e2b79e
nehadeep <erdeepjaswal@gmail.com>
2020-12-04 11:28:16 -0800

    added the sign up form and bind the value of form on submit button

---
+++ components/forms/RegisterForm.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+import {useState} from 'react'
+
+const RegisterForm  = ({onSubmit})=>{
+
+    const [form, setForm] = useState({});
+
+    const handleChange = (e) =>{
+
+        const {name, value} = e.target;
+        setForm({
+            ...form,
+            [name]: value
+        })
+
+    };
+
+    const handleSubmit = () =>{
+        onSubmit(form);
+    };
+    return(
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label htmlFor="avatar">Avatar</label>
+                <input onChange={handleChange}
+                    type="text"
+                    className="form-control" name="avatar"
+                    id="avatar" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input onChange={handleChange}
+                    type="text"
+                    className="form-control" name="username"
+                    id="username" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input onChange={handleChange}
+                    type="email"
+                    className="form-control" name="email"
+                    id="email" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input onChange={handleChange}
+                    type="password"
+                    className="form-control" name="password"
+                    id="password" />
+            </div>
+            <div className="form-group">
+                <label htmlFor="passwordConfirmation">Password Confirmation</label>
+                <input onChange={handleChange}
+                    type="password"
+                    className="form-control" name="passwordConfirmation"
+                    id="passwordConfirmation" />
+            </div>
+            <button
+                type="submit"
+                className="btn btn-main bg-blue py-2 ttu">Submit</button>
+        </form>
+    )
+};
+
+
+export default RegisterForm
--- pages/register.js
+++ pages/register.js
@@ -1,5 +1,28 @@
 
 
-const Register = () => <h1>egister page</h1>
+import React from 'react';
+import RegisterForm from "../components/forms/RegisterForm";
+
+
+const Register =  () => {
+
+    const register = (registerData) =>{
+
+    };
+
+    return (
+        <>
+            <div className="bwm-form mt-5">
+                <div className="row">
+                    <div className="col-md-5 mx-auto">
+                        <h1 className="page-title">Register</h1>
+                          <RegisterForm onSubmit = {register}/>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+
+};
 
 export default Register;
