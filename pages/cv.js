460d13791dbe44f823c1dc2c154ea14e6b06d7d0
nehadeep <erdeepjaswal@gmail.com>
2021-01-26 11:16:45 -0800

    added the resume link

--- pages/cv.js
+++ pages/cv.js
@@ -1,3 +1,16 @@
-const Cv = () => <h1>CV page</h1>
+import BaseLayout from "@/layouts/BaseLayout";
+import React from "react";
+
+const Cv = () => (
+    <BaseLayout>
+        <div className="row mt-4">
+            <div className="col-md-10 offset-md-1">
+                <iframe src="/Hardeep_Kaur_Resume2021.pdf" style={{width:'100%', height:'800px'}}>
+
+                </iframe>
+            </div>
+        </div>
+    </BaseLayout>
+);
 
 export default Cv;
--- pages/index.js
+++ pages/index.js
@@ -75,7 +75,7 @@ const Home = () => (
             <div className="card subtle-shadow no-border">
               <div className="card-body">
                 <h5 className="card-title">BACHELOR OF TECHNOLOGY IN INFORMATION TECHNOLOGY(HONS)</h5>
-                <h6 className="card-subtitle mb-2 text-muted">LOVELY PROFESSIONSL UNIVERSITY(INDIA)</h6>
+                <h6 className="card-subtitle mb-2 text-muted">LOVELY PROFESSIONAL UNIVERSITY(INDIA)</h6>
                 <p className="card-text fs-2">
                   YEAR OF PASSOUT : 2012
                 </p>
